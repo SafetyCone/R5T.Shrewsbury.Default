c31ea434f19595453020abaa4b68d235a754944c
MinexAutomation <minex.automation@gmail.com>
2019-12-27 01:27:15 -0800

    Added a current-directory-based IAppSettingsDirectoryPathProvider implementation (useful for EF tools where the executable is a dotnet tool!)

---
+++ source/R5T.Shrewsbury.Default/Code/Services/Classes/CurrentDirectoryAppSettingsDirectoryPathProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace R5T.Shrewsbury.Default
+{
+    public class CurrentDirectoryAppSettingsDirectoryPathProvider : IAppSettingsDirectoryPathProvider
+    {
+        public string GetAppSettingsDirectoryPath()
+        {
+            //// For testing in EF tools.
+            //Console.WriteLine("HERE!!!");
+            //Console.WriteLine($"Current directory:\n{Environment.CurrentDirectory}");
+            //Console.WriteLine($"Command-line args:\n{Environment.GetCommandLineArgs()[0]}");
+            //Console.WriteLine($"EntryAssembly:\n{System.Reflection.Assembly.GetEntryAssembly().Location}");
+
+            var currentDirectory = Environment.CurrentDirectory;
+
+            return currentDirectory;
+        }
+    }
+}
